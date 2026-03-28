9bf021d694732d86e28cd5e552577e8a75bf927b
kirjorjos <kirjorjos@gmail.com>
2026-03-27 23:37:51 -0400

    Add dropdowns to select between conversion options

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,8 @@ export { entityRegistry } from "lib/IntegratedDynamicsClasses/registries/entityR
 export { fluidRegistry } from "lib/IntegratedDynamicsClasses/registries/fluidRegistry";
 export { itemRegistry } from "lib/IntegratedDynamicsClasses/registries/itemRegistry";
 export { operatorRegistry } from "lib/IntegratedDynamicsClasses/registries/operatorRegistry";
+export { ASTToCodeLine, CodeLineToAST } from "lib/transformers/CodeLine";
+export { ASTToCompressed, CompressedToAST } from "lib/transformers/Compressed";
 export { ASTToCondensed, CondensedToAST } from "lib/transformers/Condensed";
 export { ASTToExpanded, ExpandedToAST } from "lib/transformers/Expanded";
+export { ASTtoJSON, JSONtoAST } from "lib/transformers/JSON";
